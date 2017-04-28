ab7355f74ad45bf310ba38f1d10b1d0d3dbc400a
Tom <schneiderthomas@gmail.com>
2017-04-27 21:26:40 -0400

    corrected some lines so files are properly downloaded

--- download_dependencies.sh
+++ download_dependencies.sh
@@ -150,11 +150,14 @@ gunzip ./chr*
 cat chrM.fa chr1.fa chr2.fa chr3.fa chr4.fa chr5.fa chr6.fa chr7.fa chr8.fa chr9.fa chr10.fa chr11.fa chr12.fa chr13.fa chr14.fa chr15.fa chr16.fa chr17.fa chr18.fa chr19.fa chr20.fa chr21.fa chr22.fa chrX.fa chrY.fa > genome.fa
 rm -f chr*
 
+cd $DIR
+
 #install bwa
 wget https://sourceforge.net/projects/bio-bwa/files/bwa-0.7.10.tar.bz2/download
 tar -vxjf download
 rm download
 cd bwa-0.7.10
+make
 
 cd $DIR
 
